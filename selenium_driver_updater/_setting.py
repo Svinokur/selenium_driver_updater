8d936a5fafe640c3f645e78edff223bfe847558f
Svinokur <stasvinokur@gmail.com>
2021-06-05 20:39:23 +0300

    see version 3.9.1 changelog

--- selenium_driver_updater/_setting.py
+++ selenium_driver_updater/_setting.py
@@ -118,7 +118,7 @@ chromiumbrowser_updater = "sudo apt-get install chromium-browser"
 chromiumchromedriver_updater = "sudo apt-get install chromedriver"
 
 class info:
-    version = "3.8.0"
+    version = "3.9.1"
 
 setting = dict(
     {
